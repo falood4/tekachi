9955f74f394b950e6953f01436f349013e2c2425
fa100da <kuriennakkara0@gmail.com>
2026-05-11 10:41:59 +0530

    render server singapore

--- tekachigeojit/lib/services/ApiConfig.dart
+++ tekachigeojit/lib/services/ApiConfig.dart
@@ -1,6 +1,8 @@
 class ApiConfig {
   static const String baseUrl = String.fromEnvironment(
     'TEKACHI_API_BASE_URL',
-    defaultValue: 'https://tekachi-ztuz.onrender.com', //render ip
+    defaultValue: 'https://tekachi-backend.onrender.com', //render ip
   );
+
+  /// - Android emulator: `http://10.0.2.2:8080
 }
